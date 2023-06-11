660a50ea027001d41cf70fced87b76799eb40b95
Angelica dos Santos <santosangelicassp@gmail.com>
2023-06-11 12:06:25 -0300

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Angelica dos Santos on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
